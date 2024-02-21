dfce1313fe75fcaf23a8b6c1aac86a4cbb745136
Blagoja Stamatovski <bazestamatovski@googlemail.com>
2024-02-21 01:08:14 +0100

    Update kotlin script

--- .github/scripts/hello.main.kts
+++ .github/scripts/hello.main.kts
@@ -3,14 +3,17 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 val environment: MutableMap<String, String> = System.getenv()
-println("Environment: $environment")
+environment.map {
+    println("Environment: ${it.key}=${it.value}")
+}
+
 val githubEnvironmentFilePath: String = environment["GITHUB_ENV"]
         ?: throw IllegalStateException("GITHUB_ENV not found in environment")
 
 println("File path: $githubEnvironmentFilePath")
 
 
-val readAllBytes = Files.readAllBytes(Path.of(githubEnvironmentFilePath))
+val readAllBytes = Files.readAllLines(Path.of(githubEnvironmentFilePath))
 
 println(readAllBytes)
 
